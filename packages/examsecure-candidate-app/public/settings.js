cad98a5e3f7724f7295e798da8f201c6c27ff158
javabipe <67403018+javabipe@users.noreply.github.com>
2022-06-27 23:39:11 -0300

    Update settings.js

--- packages/examsecure-candidate-app/public/settings.js
+++ packages/examsecure-candidate-app/public/settings.js
@@ -1,5 +1,5 @@
 window.rekognitionSettings = {
-  apiGateway: "https://c8ye3hk2oh.execute-api.sa-east-1.amazonaws.com/Prodsss",
+  apiGateway: "https://c8ye3hk2oh.execute-api.sa-east-1.amazonaws.com/Prod",
   cognitoIdentityPool: "sa-east-1:40d27fb4-7ec9-4d77-b1b6-5b1ace03a13a",
   cognitoUserPoolId: "sa-east-1_ab133LiYK",
   cognitoUserPoolClientId: "16jgdfr3ogk19fu7be1g6l9b0d",
