9a016a30a6012a31f38efa5979694a0f3066342d
javabipe <67403018+javabipe@users.noreply.github.com>
2022-07-05 18:44:47 -0300

    Update firebase_config.js

--- packages/examsecure-educator-app/src/firebase_config.js
+++ packages/examsecure-educator-app/src/firebase_config.js
@@ -5,9 +5,9 @@ import 'firebase/storage';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBDpRAq-bM-El7_YUiyV6WVFaaNgT7ubnU',
-  authDomain: 'project2-e6924.firebaseapp.com',
-  projectId: 'project2-e6924',
+  apiKey: 'AIzaSyCg2Rl7gO_CASG-0Ml6rjsRcAWkPoII6s8',
+  authDomain: 'algar-1616501032135.firebaseapp.com',
+  projectId: 'algar-1616501032135',
   storageBucket: 'project2-e6924.appspot.com',
   messagingSenderId: '361613734874',
   appId: '1:361613734874:web:40d4f4f1d7cb4a04fa86e3',
