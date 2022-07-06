935efc16b622d3d1687452e055eb1580ce87452b
javabipe <67403018+javabipe@users.noreply.github.com>
2022-07-05 22:13:31 -0300

    Update settings.js

--- packages/examsecure-candidate-app/public/settings.js
+++ packages/examsecure-candidate-app/public/settings.js
@@ -1,7 +1,7 @@
 window.rekognitionSettings = {
-  apiGateway: "https://gasom6hkgl.execute-api.us-east-1.amazonaws.com/Prod/faces/index",
-  cognitoIdentityPool: "us-east-1:913f6cfd-8207-45f0-842e-cd5e047bfe4a",
-  cognitoUserPoolId: "us-east-1_GjRF9HC0w",
-  cognitoUserPoolClientId: "2u72im60asf0o7s0chqcqpsmop",
+  apiGateway: "https://hv06h4egdk.execute-api.us-east-1.amazonaws.com",
+  cognitoIdentityPool: "us-east-1:17c984e0-ef84-41c8-893b-78237db6c449",
+  cognitoUserPoolId: "us-east-1_t3xXuklGz",
+  cognitoUserPoolClientId: "4k256ku2hlbfnq5jsj1sfsvcsr",
   region: "us-east-1",
 };
