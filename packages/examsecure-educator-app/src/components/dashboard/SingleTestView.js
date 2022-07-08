b9fe93a6277e35918a0e754f2db2f7a7d8773464
javabipe <67403018+javabipe@users.noreply.github.com>
2022-07-08 10:48:08 -0300

    Update SingleTestView.js

--- packages/examsecure-educator-app/src/components/dashboard/SingleTestView.js
+++ packages/examsecure-educator-app/src/components/dashboard/SingleTestView.js
@@ -11,21 +11,21 @@ const SingleTestView = ({ test, id }) => {
         </div>
         <div className="dash-single-test-view-left-test-details">
           <div className="dash-single-test-view-left-test-details-row">
-            <div>Started at {test.test_starts_at}</div>
-            <div>Ends at {test.test_ends_at}</div>
+            <div>Começa em {test.test_starts_at}</div>
+            <div>Termina em {test.test_ends_at}</div>
           </div>
           <div className="dash-single-test-view-left-test-details-row">
-            <div>20 Candidates Submitted</div>
-            <div>20 Questions, {test.test_duration} minutes</div>
+            <div>10 Alunos terminaram</div>
+            <div>10 Questões, {test.test_duration} minutos</div>
           </div>
         </div>
       </div>
       <div className="dash-single-test-view-right">
         <Link to={`/proctor-mode/test_overview?test=${id}`}>
-          <Button variant={'secondary'} label={'More Details'} />
+          <Button variant={'secondary'} label={'Mais detalhes'} />
         </Link>
         <Link to={`/proctor-mode/proctor_dashboard?test=${id}`}>
-          <Button variant={'secondary'} label={'Proctor Mode'} />
+          <Button variant={'secondary'} label={'Modo supervisor'} />
         </Link>
       </div>
     </div>
